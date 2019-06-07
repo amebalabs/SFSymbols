aaa3776529f552b27c211766d9a0487a9cbfa3b0
melonamin <alexandr.mazanov@gmail.com>
2019-06-06 20:08:09 -0400

    Force cast for easier use of initializer
    NOt sure about this one

--- Sources/SFSymbols/UIImage+Extension.swift
+++ Sources/SFSymbols/UIImage+Extension.swift
@@ -2,7 +2,7 @@ import UIKit
 
 @available(iOS 13.0,watchOS 6,tvOS 13, *)
 public extension UIImage {
-    convenience init?(systemSymbol: SFSymbol) {
-        self.init(systemName: systemSymbol.rawValue)
+    convenience init(systemSymbol: SFSymbol) {
+        self.init(systemName: systemSymbol.rawValue)!
     }
 }
