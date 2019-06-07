551ce87c6cad651c5ed67edb7895eccfcf0a64e5
melonamin <alexandr.mazanov@gmail.com>
2019-06-06 20:11:08 -0400

    Of course this should be public

--- Sources/SFSymbols/Image+Extension.swift
+++ Sources/SFSymbols/Image+Extension.swift
@@ -1,7 +1,7 @@
 import SwiftUI
 
 @available(iOS 13.0,watchOS 6,tvOS 13, *)
-extension Image {
+public extension Image {
     init(systemSymbol: SFSymbol) {
         self.init(uiImage: UIImage(systemSymbol: systemSymbol))
     }
